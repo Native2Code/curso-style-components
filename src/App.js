5884291189ca8363dd61ed93e69e667df1840721
Esteban Solorzano <david.solorzano@atlanticsoft.co>
2019-08-20 10:10:18 -0500

    Agregando estilos al button y creando un container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+	background: palevioletred;
+	border-radius: 3px;
+	border: none;
+	color: white;
+	margin: 0 1em;
+	padding: 1em 3em;
+`;
+
+const StyledContainer = styled.div`
+	padding: 4em;
+	min-height: 100vh;
+	min-width: 100vw;
+	background: papayawhip;
+`;
 
 function App() {
-	return <button>Button</button>;
+	return (
+		<StyledContainer>
+			<StyledButton>Button</StyledButton>
+		</StyledContainer>
+	);
 }
 
 export default App;
